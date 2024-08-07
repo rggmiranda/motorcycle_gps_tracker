c73ab9d651118296d39f18a3f807a68515291c68
rggmiranda <rggmiranda@gmail.com>
2024-08-07 13:15:14 -0300

    Navbar ready

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -8,75 +8,81 @@ import { useSession } from "next-auth/react";
 import { isMobile } from "react-device-detect";
 import Spinner from "react-bootstrap/Spinner";
 import "./Navbar.css";
+import Image from "next/image";
 
 function CollapsibleExample() {
   const { status, data: session } = useSession();
   return (
-    <Navbar collapseOnSelect expand="xl" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">
-          <img
-            src="/logo.png"
-            width="40rem"
-            height="40rem"
-            className="d-inline-block align-center mx-1"
-          />{" "}
-          Mottrack
+          <div>
+            <img
+              src="/logo.png"
+              width="40rem"
+              height="40rem"
+              className="d-inline-block align-center mx-1"
+              alt="Logo"
+            />{" "}
+            Mottrack
+          </div>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav d-flex justify-content-between">
-          <Nav >
-            <Nav.Link eventKey={2} href="#dashboard">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              className={`${isMobile ? "my-1" : ""}`}
+              eventKey={2}
+              href="#dashboard"
+            >
               Dashboard
             </Nav.Link>
-            <Nav.Link href="#store">Store</Nav.Link>
+            <Nav.Link className={`${isMobile ? "my-1" : ""}`} href="#Store">
+              Store
+            </Nav.Link>
           </Nav>
-          {/* <Nav > */}
-          {status === "authenticated" && (
-            <NavDropdown
-              className=""
-              title={
-                (!isMobile && (
-                  <div
-                  // className="d-inline-block"
-                  className="d-flex align-items-center justify-content-center"
-                  >
-                    <img
-                      src={session?.user?.image!}
-                      style={{
-                        width: "30px",
-                        height: "30px",
-                      }}
-                    />
-                  </div>
-                )) ||
-                (isMobile && (
-                  <div className="d-inline-block align-center my-1 text">
-                    {session?.user?.name}
-                  </div>
-                ))
-              }
-              id="collapsible-nav-dropdown"
-              align={isMobile ? "start" : "end"}
-            >
-              <NavDropdown.Item href="#action/3.1">
-                User configuration
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/api/auth/signout">
-                Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-          )}
-          {status === "loading" && (
-            <Spinner animation="grow" variant="secondary" />
-          )}
-          {status === "unauthenticated" && (
-            <a href="/api/auth/signin" className="text">
-              Login
-            </a>
-          )}
-          {/* </Nav> */}
+          <Nav className={`${isMobile ? "my-1" : ""}`}>
+            {status === "authenticated" && (
+              <NavDropdown
+                className=""
+                title={
+                  (!isMobile && (
+                    <div className="d-flex d-inline-block align-items-center justify-content-center">
+                      <img
+                        src={session?.user?.image!}
+                        width={30}
+                        height={30}
+                        className="d-inline-block mx-1 "
+                      />
+                    </div>
+                  )) ||
+                  (isMobile && (
+                    <div className=" align-center text">
+                      {session?.user?.name}
+                    </div>
+                  ))
+                }
+                id="collapsible-nav-dropdown"
+                align={isMobile ? "start" : "end"}
+              >
+                <NavDropdown.Item href="#user/config">
+                  User configuration
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/api/auth/signout">
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+            {status === "loading" && (
+              <Spinner animation="grow" variant="secondary" className="mx-2" />
+            )}
+            {status === "unauthenticated" && (
+              <a href="/api/auth/signin" className="text">
+                Login
+              </a>
+            )}
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
